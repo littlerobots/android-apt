6b670681ac48cbfc15d0afe9c012aa7cf43918de
Hugo Visser <hugo@littlerobots.nl>
2014-06-08 11:42:09 +0200

    Make sure that project dependencies in the apt configuration are packaged before the variant compiles. This should make sure that annotation processors that are contained in a multi project setup are run as part of the build. Fixes #11 #12

--- src/main/groovy/com/neenbedankt/gradle/androidapt/AndroidAptPlugin.groovy
+++ src/main/groovy/com/neenbedankt/gradle/androidapt/AndroidAptPlugin.groovy
@@ -3,6 +3,8 @@ package com.neenbedankt.gradle.androidapt
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.ProjectConfigurationException
+import org.gradle.api.artifacts.ProjectDependency
+import org.gradle.api.tasks.bundling.AbstractArchiveTask
 
 class AndroidAptPlugin implements Plugin<Project> {
     void apply(Project project) {
@@ -31,6 +33,14 @@ class AndroidAptPlugin implements Plugin<Project> {
                 project.apt.aptArguments.variant = variant
                 project.apt.aptArguments.project = project
                 project.apt.aptArguments.android = project.android
+
+                projectDependencies = project.configurations.getByName('apt').allDependencies.withType(ProjectDependency.class)
+                // There must be a better way, but for now grab the tasks that produce some kind of archive and make sure those
+                // run before this javaCompile. Packaging makes sure that processor meta data is on the classpath
+                projectDependencies.each { p ->
+                    def archiveTasks = p.dependencyProject.tasks.withType(AbstractArchiveTask.class)
+                    archiveTasks.each { t -> variant.javaCompile.dependsOn t.path }
+                }
 
                 variant.javaCompile.options.compilerArgs+=project.apt.arguments()
 
--- src/test/groovy/com/neenbedankt/gradle/androidapt/AndroidAptPluginTest.groovy
+++ src/test/groovy/com/neenbedankt/gradle/androidapt/AndroidAptPluginTest.groovy
@@ -15,4 +15,33 @@ class AndroidAptPluginTest {
         } catch (expected) {
         }
     }
+
+    @Test
+    public void testProjectAptDependency() {
+        Project root = ProjectBuilder.builder().build();
+        Project testProject = ProjectBuilder.builder().withName(":test").withParent(root).build();
+        testProject.apply plugin: 'java'
+        Project p = ProjectBuilder.builder().withParent(root).build()
+        p.apply plugin: 'android'
+        p.apply plugin: 'android-apt'
+        p.dependencies {
+            apt testProject
+        }
+        p.android {
+            compileSdkVersion 19
+            buildToolsVersion "19.1"
+
+            defaultConfig {
+                minSdkVersion 14
+                targetSdkVersion 19
+                versionCode 1
+                versionName "1.0"
+            }
+        }
+        p.evaluate()
+        // FIXME assert that the test:jar task is added as a dependency
+//        p.android.applicationVariants.all { v ->
+//            dependencies = v.javaCompile.taskDependencies.getDependencies(v.javaCompile)
+//        }
+    }
 }
